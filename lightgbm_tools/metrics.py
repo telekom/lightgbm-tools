34bfe30f139adc648db33979f6509444d4f5a4dc
Philip May <philip@may.la>
2022-09-19 13:42:24 +0200

    add metrics function definitions

--- lightgbm_tools/metrics.py
+++ lightgbm_tools/metrics.py
@@ -10,9 +10,14 @@ from typing import Callable, List
 
 import lightgbm as lgbm
 import numpy as np
-
-
-# from sklearn.metrics import accuracy_score, average_precision_score, f1_score
+from sklearn.metrics import (
+    accuracy_score,
+    average_precision_score,
+    f1_score,
+    precision_score,
+    recall_score,
+    roc_auc_score,
+)
 
 
 @dataclass
@@ -32,10 +37,12 @@ def binary_eval_callback_factory(lightgbm_eval_functions: List[LightGbmEvalFunct
         """TODO: add docstring."""
         assert y_pred.ndim == 1
         y_true = data.get_label()
-        y_pred_binary = np.round(y_pred)
+        y_pred_binary = None  # we do lasy init here (see below)
         results = []
         for lightgbm_eval_function in lightgbm_eval_functions:
             if lightgbm_eval_function.needs_binary_predictions:
+                if y_pred_binary is None:  # we do lasy init here
+                    y_pred_binary = np.round(y_pred)
                 result = lightgbm_eval_function.function(y_true, y_pred_binary)
             else:
                 result = lightgbm_eval_function.function(y_true, y_pred)
@@ -45,3 +52,48 @@ def binary_eval_callback_factory(lightgbm_eval_functions: List[LightGbmEvalFunct
         return results
 
     return binary_eval_callback
+
+
+f1_score = LightGbmEvalFunction(
+    name="f1",
+    function=f1_score,
+    is_higher_better=True,
+    needs_binary_predictions=True,
+)
+
+accuracy_score = LightGbmEvalFunction(
+    name="accuracy",
+    function=accuracy_score,
+    is_higher_better=True,
+    needs_binary_predictions=True,
+)
+
+average_precision_score = LightGbmEvalFunction(
+    name="average_precision",
+    function=average_precision_score,
+    is_higher_better=True,
+    needs_binary_predictions=False,
+)
+
+roc_auc_score = LightGbmEvalFunction(
+    name="roc_auc",
+    function=roc_auc_score,
+    is_higher_better=True,
+    needs_binary_predictions=False,
+)
+
+
+recall_score = LightGbmEvalFunction(
+    name="recall",
+    function=recall_score,
+    is_higher_better=True,
+    needs_binary_predictions=True,
+)
+
+
+precision_score = LightGbmEvalFunction(
+    name="precision",
+    function=precision_score,
+    is_higher_better=True,
+    needs_binary_predictions=True,
+)
